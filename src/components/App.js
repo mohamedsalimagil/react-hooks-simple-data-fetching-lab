eb8821d4e58c92d3017cd0854e478bdcc63960b9
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-08-28 18:48:50 +0300

    latest

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { useEffect, useState } from "react"; 
+
+function App() {
+    const [dogImage, setDogImage]= useState(null);
+    const [loading, setLoading] = useState(true);
+
+        useEffect(() => {
+        const fetchDogImage = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch("https://dog.ceo/api/breeds/image/random");
+                const data = await response.json();
+                setDogImage(data.message);
+            } catch (error) {
+                console.error("Error fetching dog image:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDogImage();
+    }, []);
+    if (loading) {
+        return <p>Loading...</p>;// Add a loading state
+    }
+
+    return (
+        <div>
+            <img src={dogImage} alt="A Random Dog" />// Display the dog image
+        </div>
+    );
+}
+export default App
